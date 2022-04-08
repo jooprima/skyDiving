0f84b9957f5ed2c7116c504a21a631a8bb2b9852
jooprima <jooprima@gmail.com>
2022-04-09 02:11:13 +0700

    create random clouds

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 const airplane = document.getElementById("airplane");
 const manJump = document.getElementById("man-jump");
 const canvas = document.getElementById("canvas");
+const clouds = document.getElementById("clouds");
+const totalClouds = 5;
+
+//random number betwen min and max
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function setClouds() {
+  for (let i = 1; i <= totalClouds; i++) {
+    let cloud = document.createElement("div");
+    cloud.id = "cloud" + i;
+    cloud.classList.add("cloud" + random(1, 5));
+    clouds.appendChild(cloud);
+
+    //set position
+    cloud.style.left = random(10, window.innerWidth) + "px";
+    cloud.style.top = random(0, innerHeight / 2) + "px";
+  }
+}
 
 function setBackground() {
   canvas.style.width = window.innerWidth + "px";
@@ -72,5 +92,6 @@ function animate() {
   );
 }
 
+setClouds();
 setBackground();
 animate();
